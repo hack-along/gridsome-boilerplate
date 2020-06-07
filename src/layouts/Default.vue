dbeb6520fbe0541057c5a1b8b9107ad82e3c433a
Maija <hello@maijagrudule.com>
2020-06-07 22:44:56 +0200

    🚀 Init + ✈️ tailwind & scss setup 🖼️ card coponent

---
+++ src/layouts/Default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <header class="header">
+      <nav-bar />
+    </header>
+    <main class="container flex-grow">
+      <slot />
+    </main>
+    <g-footer />
+  </div>
+</template>
+
+
